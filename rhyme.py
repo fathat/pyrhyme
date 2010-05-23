539224a9b8f3f364dafb331266e68863abaf3006
Ian Overgard <ian@Ian-Overgards-iMac.local>
2010-05-23 15:49:35 -0600

    added readme

--- rhyme.py
+++ rhyme.py
@@ -5,20 +5,22 @@ import sys
 _conn = sql.connect('data/rhyme.db')
 
 def rhymes_with(word):
-	"""Returns a list of words that rhyme, or None if no words rhyme."""
-	global _conn
-	cursor = _conn.execute("select * from words where word=?", (word.lower(),))
-	row = cursor.fetchone()
-	if not row: return None
-	word, sound, key = row
-	cursor = _conn.execute("select * from rhymes where sound=?", (sound,))
-	sound, words = cursor.fetchone()
-	#return all the matching words, if a word has a (n) on it, clip it off.
-	return [x.split('(')[0] for x in words.split()]
+    """Returns a list of words that rhyme, or [] if no words rhyme."""
+    global _conn
+    cursor = _conn.execute("select * from words where word=?", (word.lower(),))
+    row = cursor.fetchone()
+    if not row: return []
+    word, sound, key = row
+    cursor = _conn.execute("select * from rhymes where sound=?", (sound,))
+    sound, words = cursor.fetchone()
+    
+    #return all the matching words. If a word has a (n) on it, clip it off,
+    #and also don't return the original word
+    return [x.split('(')[0] for x in words.split() if x.lower() != word.lower()]
 
 def main():
-	for word in sys.argv[1:]:
-		print '%s: %s' % (word, ', '.join(rhymes_with(word)))
-		
+    for word in sys.argv[1:]:
+        print '%s: %s' % (word, ', '.join(rhymes_with(word)))
+        
 
 main() if __name__=='__main__' else None
